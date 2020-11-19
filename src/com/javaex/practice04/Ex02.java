0c38b0d3990b72b2b101b72d502955de00d52828
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-19 16:04:19 +0900

    문제 Ex02 거꾸로 출력하기

---
+++ src/com/javaex/practice04/Ex02.java
@@ -0,0 +1,17 @@
+package com.javaex.practice04;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		double dArray[] = new double[3];
+		
+		dArray[0] = 1.2;
+		dArray[1] = 3.3;
+		dArray[2] = 6.7;
+				
+		for(int i=2;i>=0;i--) {
+			System.out.println(dArray[i]);
+		}
+	}
+
+}
