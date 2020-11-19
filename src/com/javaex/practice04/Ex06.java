d5beb1ec05aec78687f10ba63cefb1d39e858ae2
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-19 16:54:10 +0900

    문제 Ex07 (' ' -> ','로 변환)

---
+++ src/com/javaex/practice04/Ex06.java
@@ -0,0 +1,21 @@
+package com.javaex.practice04;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		
+		for(int i=0;i<c.length;i++) {
+			System.out.print(c[i]);
+		}System.out.println("");
+		
+		for(int j=0;j<c.length;j++) {
+			if(c[j]==' ') {
+				c[j] = ',';
+			}
+			System.out.print(c[j]);
+		}
+
+	}
+
+}
